9ae805a0ed25b249db4ed9752845b85920eb4405
Sardius <0xsardius@gmail.com>
2023-05-09 19:57:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+async function fetchData() {
+  await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+  });
+}
