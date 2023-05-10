b2ccadf2cdde4d7da26ac7da7bae1d8f2b162629
Sardius <0xsardius@gmail.com>
2023-05-09 20:23:12 -0400

    MVP finalized

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+require("dotenv").config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
 async function fetchData() {
-  await fetch("https://api.openai.com/v1/chat/completions", {
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "user",
+          content:
+            "gm! Please give me 10 decentralized app ideas for sustainable small projects!",
+        },
+      ],
+    }),
   });
+  const data = await response.json();
+  console.log(data);
 }
+
+fetchData();
